647b9045fd311ef8d228d1e93afe81d01780c6e7
Mr. T <tholkingk@smtexas.org>
2017-01-06 10:59:30 -0600

    Starter code

---
+++ Magpie/src/activity3/MagpieRunner3.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package activity3;
+import java.util.Scanner;
+
+/**
+ * A simple class to run the Magpie class.
+ * @author Mr. T
+ * @version January 2017
+ */
+public class MagpieRunner3
+{
+
+	/**
+	 * Create a Magpie, give it user input, and print its replies.
+	 */
+	public static void main(String[] args)
+	{
+		Magpie3 maggie = new Magpie3();
+		
+		System.out.println (maggie.getGreeting());
+		Scanner in = new Scanner (System.in);
+		String statement = in.nextLine();
+		
+		while (!statement.equals("Bye"))
+		{
+			System.out.println (maggie.getResponse(statement));
+			statement = in.nextLine();
+		}
+	}
+
+}
